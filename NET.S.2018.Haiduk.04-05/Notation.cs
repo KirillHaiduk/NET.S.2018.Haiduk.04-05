42dabda27255c9009567a34c23f335ddde672766
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-03-26 19:38:04 +0300

    Add files via upload

---
+++ NET.S.2018.Haiduk.04-05/Notation.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace NET.S._2018.Haiduk._04_05
+{
+    /// <summary>
+    /// Class that describes notation
+    /// </summary>
+    public class Notation
+    {
+        private string alphabet = "0123456789ABCDEF";
+
+        public Notation(int basis)
+        {
+            if (basis < 2 || basis > 16)
+            {
+                throw new ArgumentException($"{nameof(basis)} must be from 2 to 16.");
+            }
+
+            Basis = basis;
+        }
+                
+        public int Basis { get; }
+
+        public string Alphabet => alphabet.Substring(0, Basis);
+    }
+}
---
+++ NET.S.2018.Haiduk.04-05/StringExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace NET.S._2018.Haiduk._04_05
+{
+    /// <summary>
+    /// Class that contains extension method for converting from 2 to 16 notation into decimal
+    /// </summary>
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Method for converting from 2 to 16 notation into decimal
+        /// </summary>
+        /// <param name="source">Number in some notation as a string</param>
+        /// <param name="notation">Description of notation</param>
+        /// <returns>Decimal number</returns>
+        public static int ToDecimalConverter(this string source, Notation notation)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException($"{nameof(source)} is empty.");
+            }
+
+            int result = 0;
+
+            for (int i = 0, j = source.Length - 1; i < source.Length; i++)
+            {
+                result += (int)Math.Pow(notation.Basis, j - i) * notation.Alphabet.IndexOf(source[i].ToString().ToUpper());
+            }
+
+            return result;
+        }
+    }
+}
