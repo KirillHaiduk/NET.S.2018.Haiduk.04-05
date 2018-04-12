e65a7f6b1821c0f104b1229697d845dabdfed026
KirillHaiduk <33031000+KirillHaiduk@users.noreply.github.com>
2018-04-12 13:50:48 +0300

    Update GCDMethods.cs

--- NET.S.2018.Haiduk.04-05/GCDMethods.cs
+++ NET.S.2018.Haiduk.04-05/GCDMethods.cs
@@ -2,17 +2,17 @@ using System;
 
 namespace NET.S._2018.Haiduk._04_05
 {
-    public static class GCDMethods
-    {        
+    public class GCDMethods
+    {
         #region Public methods
+
         /// <summary>
-        /// Method that calculates GCD for 2 numbers using type of accepted delegate
+        /// Method that calculates GCD for 2 numbers using Euclid algorythm
         /// </summary>
-        /// <param name="gcdDelegate">Delegate type for calculating GCD</param>
         /// <param name="a">1st number</param>
         /// <param name="b">2nd number</param>
         /// <returns>GCD of given numbers</returns>
-        public static int FindGCD(Func<int, int, int> gcdDelegate, int a, int b)
+        public static int EuclidGCD(int a, int b)
         {
             if (a == 0)
             {
@@ -23,64 +23,6 @@ namespace NET.S._2018.Haiduk._04_05
                 return a;
             }
 
-            return gcdDelegate(a, b);
-        }
-
-        /// <summary>
-        /// Method that calculates GCD for 3 numbers using type of accepted delegate
-        /// </summary>
-        /// <param name="gcdDelegate">Delegate type for calculating GCD</param>
-        /// <param name="a">1st number</param>
-        /// <param name="b">2nd number</param>
-        /// <param name="c">3rd number</param>
-        /// <returns>GCD of given numbers</returns>
-        public static int FindGCD(Func<int, int, int> gcdDelegate, int a, int b, int c)
-        {
-            int[] array = { a, b, c };
-            int gcd = array[0];
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                gcd = gcdDelegate(gcd, array[i + 1]);
-            }
-
-            return gcd;
-        }
-
-        /// <summary>
-        /// Method that calculates GCD for array of numbers using type of accepted delegate
-        /// </summary>
-        /// <param name="gcdDelegate">Delegate type for calculating GCD</param>
-        /// <param name="array">Array of 2, 3 or more numbers</param>
-        /// <returns>GCD of given numbers</returns>
-        public static int FindGCD(Func<int, int, int> gcdDelegate, params int[] array)
-        {
-            if (array.Length == 0 || array is null)
-            {
-                throw new ArgumentNullException($"{nameof(array)} is empty.");
-            }
-
-            if (array.Length == 1)
-            {
-                return array[0];
-            }
-
-            int gcd = array[0];
-            for (int i = 0; i < array.Length - 1; i++)
-            {
-                gcd = gcdDelegate(gcd, array[i + 1]);
-            }
-
-            return gcd;
-        }
-
-        /// <summary>
-        /// Supporting method that calculates GCD of 2 given numbers for Euclid algorythm
-        /// </summary>
-        /// <param name="a">1st give number</param>
-        /// <param name="b">2nd given number</param>
-        /// <returns>GCD of 2 numbers</returns>
-        public static int FindEuclidGCD(int a, int b)
-        {
             while (b != 0)
             {
                 b = a % (a = b);
@@ -90,13 +32,38 @@ namespace NET.S._2018.Haiduk._04_05
         }
 
         /// <summary>
-        /// Supporting method that calculates GCD of 2 given numbers for Stein algorythm
+        /// Method that calculates GCD for 3 numbers using Euclid algorythm
         /// </summary>
-        /// <param name="a">1st give number</param>
-        /// <param name="b">2nd given number</param>
-        /// <returns>GCD of 2 numbers</returns>
-        public static int FindSteinGCD(int a, int b)
+        /// <param name="a">1st number</param>
+        /// <param name="b">2nd number</param>
+        /// <param name="c">3rd number</param>
+        /// <returns>GCD of given numbers</returns>
+        public static int EuclidGCD(int a, int b, int c) => FindGCD(EuclidGCD, a, b, c);
+
+        /// <summary>
+        /// Method that calculates GCD using Euclid algorythm
+        /// </summary>
+        /// <param name="array">Array of 2, 3 or more numbers</param>
+        /// <returns>GCD of given numbers</returns>
+        public static int EuclidGCD(params int[] array) => FindGCD(EuclidGCD, array);
+
+        /// <summary>
+        /// Method that calculates GCD for 2 numbers using Stein algorythm (Euclid binary algorythm)
+        /// </summary>
+        /// <param name="a">1st number</param>
+        /// <param name="b">2nd number</param>
+        /// <returns>GCD of given numbers</returns>
+        public static int SteinGCD(int a, int b)
         {
+            if (a == 0)
+            {
+                return b;
+            }
+            else if (b == 0)
+            {
+                return a;
+            }
+
             int k = 1;
             a = Math.Abs(a);
             b = Math.Abs(b);
@@ -130,6 +97,64 @@ namespace NET.S._2018.Haiduk._04_05
             }
 
             return b * k;
+        }
+
+        /// <summary>
+        /// Method that calculates GCD for 2 numbers using Stein algorythm (Euclid binary algorythm)
+        /// </summary>
+        /// <param name="a">1st number</param>
+        /// <param name="b">2nd number</param>
+        /// <param name="c">3rd number</param>
+        /// <returns>GCD of given numbers</returns>
+        public static int SteinGCD(int a, int b, int c) => FindGCD(SteinGCD, a, b, c);
+
+        /// <summary>
+        /// Method that calculates GCD using Stein algorythm (Euclid binary algorythm)
+        /// </summary>
+        /// <param name="array">Array of 2, 3 or more numbers</param>
+        /// <returns>GCD of given numbers</returns>
+        public static int SteinGCD(params int[] array) => FindGCD(SteinGCD, array);
+
+        #endregion
+
+        #region Private Methods        
+
+        private static int FindGCD(Func<int, int, int> gcdDelegate, int a, int b, int c)
+        {
+            if (gcdDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(gcdDelegate));
+            }
+
+            int[] array = { a, b, c };
+            int gcd = array[0];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                gcd = gcdDelegate(gcd, array[i + 1]);
+            }
+
+            return gcd;
+        }
+
+        private static int FindGCD(Func<int, int, int> gcdDelegate, params int[] array)
+        {
+            if (array == null || array.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+                        
+            if (gcdDelegate == null)
+            {
+                throw new ArgumentNullException(nameof(gcdDelegate));
+            }
+
+            int gcd = array[0];
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                gcd = gcdDelegate(gcd, array[i + 1]);
+            }
+
+            return gcd;
         }
         #endregion
     }
